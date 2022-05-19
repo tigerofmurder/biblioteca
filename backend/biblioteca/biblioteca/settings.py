68f03f16d08d08a95765fc59f90f7c2366144953
Jonathan Brandon Cueva Flores <tigerofmurder@gmail.com>
2022-05-18 23:07:10 -0500

    Update settings.py

--- backend/biblioteca/biblioteca/settings.py
+++ backend/biblioteca/biblioteca/settings.py
@@ -131,7 +131,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100
 }
-
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 SIMPLE_JWT = {
